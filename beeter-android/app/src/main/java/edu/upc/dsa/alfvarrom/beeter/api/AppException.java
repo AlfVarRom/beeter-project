1ba52c07f0e1b205dbfc4dc6992b9d2eee8919a9
Alfredo Varela <Alfredo Varela@Alfredo>
2014-11-26 12:17:33 +0100

    commit con android

---
+++ beeter-android/app/src/main/java/edu/upc/dsa/alfvarrom/beeter/api/AppException.java
@@ -0,0 +1,11 @@
+package edu.upc.dsa.alfvarrom.beeter.api;
+
+public class AppException extends Exception {
+    public AppException() {
+        super();
+    }
+
+    public AppException(String detailMessage) {
+        super(detailMessage);
+    }
+}
